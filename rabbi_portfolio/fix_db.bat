b2c782f0cc9ee23df67047265e0ce7306136d624
Rabbi <rabbiprimon00000@gmail.com>
2026-03-15 10:19:39 +0600

    Updated everything

--- rabbi_portfolio/fix_db.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-where python > migration_out.txt 2>&1
-python --version >> migration_out.txt 2>&1
-python manage.py makemigrations myapp >> migration_out.txt 2>&1
-python manage.py migrate >> migration_out.txt 2>&1
